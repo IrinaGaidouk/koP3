b62a1037906f6dd8c7d5342a188ca88cd678c226
app-krolik <coldusagi@protonmail.com>
2019-10-26 21:54:57 +0200

    HomeActivity

--- app/src/main/java/com/example/kop3/MainActivity.java
+++ app/src/main/java/com/example/kop3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kop3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,8 +17,10 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     private EditText itemET;
-    private Button btn;
     private ListView itemsList;
+
+    private Button btn_addItem;
+    private Button btn_Start;
     
     private ArrayList<String> items;
     private ArrayAdapter<String> adapter;
@@ -28,7 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         itemET = findViewById(R.id.item_edit_text);
-        btn = findViewById(R.id.item_btn);
+        btn_addItem = findViewById(R.id.item_btn_additem);
+        btn_Start = findViewById(R.id.item_btn_start);
         itemsList = findViewById(R.id.items_list);
 
         items = FileHelper.readData(this);
@@ -36,14 +40,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         itemsList.setAdapter(adapter);
 
-        btn.setOnClickListener(this);
+        btn_addItem.setOnClickListener(this);
         itemsList.setOnItemClickListener(this);
+
+        btn_Start.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.item_btn:
+            case R.id.item_btn_additem:
                 String itemEntered = itemET.getText().toString();
                 adapter.add(itemEntered);
                 itemET.setText("");
@@ -51,7 +57,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this, "Item Added (toast)", Toast.LENGTH_SHORT).show();
                 
                 break;
-                
+
+                case R.id.item_btn_start:
+                Intent intent_to_home = new Intent(this, HomeActivity.class);
+                startActivity(intent_to_home);
+
         }
     }
 
